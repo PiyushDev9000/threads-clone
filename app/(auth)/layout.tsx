3873742f4d26b56e20a7cd3dcc16ee9643483896
Piyush Narwal <piyushSWE009@gmail.com>
2023-09-30 02:49:19 +0530

    Homepage Complete

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "../globals.css";
 export const metadata = {
-  title: "Threads",
-  description: "A Next.js 13 Meta Threads Application",
+  title: "Momentify",
+  description: "A Next.js 13 Meta Momentify Application",
 };
 
 const inter = Inter({ subsets: ["latin"] });
